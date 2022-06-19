b41e15f1129f42956a3b41b95b262eb503b2306e
Saurav sanjel <sauravsanjelgg@gmail.com>
2022-06-19 12:16:51 +0545

    frontend menton challange complete

---
+++ README.md
@@ -0,0 +1,6 @@
+# Frontend Mentor - Expenses chart component solution
+
+This is a solution to the [Expenses chart component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/expenses-chart-component-e7yJBUdjwt).
+
+Helpful resource:
+Mdn references
---
+++ index.js
@@ -0,0 +1,60 @@
+function main(data) {
+  const weekday = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+  const d = new Date();
+  let day = weekday[d.getDay()];
+  const maxA = (arr) => {
+    let max = 0;
+    arr.forEach((element) => {
+      if (element.amount > max) {
+        max = element.amount;
+      }
+    });
+    return max;
+  };
+
+  const higest = maxA(data);
+
+  const days = document.getElementById('days').children;
+  for (var i = 0; i < days.length; i++) {
+    const calculation = (data[i].amount / higest) * 100;
+    days[i].lastElementChild.style.background = '#ec755d';
+    days[i].lastElementChild.style.height = `${calculation}px`;
+    days[i].firstChild.style.height = `${120 - calculation}px`;
+    days[i].lastElementChild.style.width = '30px';
+
+    if (data[i].day === day) {
+      days[i].lastElementChild.style.background = '#76b5bc';
+    }
+  }
+}
+const data = [
+  {
+    day: 'mon',
+    amount: 17.45,
+  },
+  {
+    day: 'tue',
+    amount: 34.91,
+  },
+  {
+    day: 'wed',
+    amount: 52.36,
+  },
+  {
+    day: 'thu',
+    amount: 31.07,
+  },
+  {
+    day: 'fri',
+    amount: 23.39,
+  },
+  {
+    day: 'sat',
+    amount: 43.28,
+  },
+  {
+    day: 'sun',
+    amount: 25.48,
+  },
+];
+main(data);
